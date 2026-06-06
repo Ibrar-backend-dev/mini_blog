496fe8bf111c1b7d2ada250ee2cd14ebb36d0d58
Ibrar shahzad <ibrarshahzad.dev@gmail.com>
2026-06-06 18:23:42 +0500

    Seprated Private   and Public post

--- apps/posts/views.py
+++ apps/posts/views.py
@@ -1,5 +1,6 @@
 ﻿from rest_framework import permissions
 from rest_framework.viewsets import ModelViewSet
+from django.db.models import Q
 
 from .models import Post
 from.serializers import PostSerializer
@@ -8,7 +9,14 @@ class PostViewSet(ModelViewSet):
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticated]
 
-    queryset = Post.objects.all()
+    def get_queryset(self):
+        user = self.request.user
+
+        if user.is_authenticated:
+            return Post.objects.filter(Q(is_private = False)|Q(author=user))
+        
+        return Post.objects.filter(is_private = False)
+    
 
     def perform_create(self, serializer):
 
