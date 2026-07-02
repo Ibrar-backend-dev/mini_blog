3ed286ec32e695824495143aea3d58bcbb9cb49c
taimoor-mumtaz <taimoor.mumtaz@terafort.org>
2026-07-02 15:11:09 +0500

    Refactor user views for improved readability and consistency. Adjust formatting, enhance response structure, and clean up commented-out code in Signup, VerifyEmail, Login, Profile, and ProfilePhoto views.

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,4 +1,4 @@
-from rest_framework import permissions,status
+from rest_framework import permissions, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.contrib.auth import get_user_model
@@ -12,61 +12,68 @@ from apps.address.serializers import AddressSerializer
 
 
 from .services import send_verification_email
-from .serializers import ProfilePhotoSerializer, SignupSerializer, LoginSerializer, UserProfileSerializer
+from .serializers import (
+    ProfilePhotoSerializer,
+    SignupSerializer,
+    LoginSerializer,
+    UserProfileSerializer,
+)
 
 User = get_user_model()
+
 
 class SignupView(APIView):
     permission_classes = [permissions.AllowAny]
 
-    def post(self,request):
-        serializer = SignupSerializer(data = request.data)
-        serializer.is_valid(raise_exception = True)
+    def post(self, request):
+        serializer = SignupSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
 
         user = serializer.save()
 
         send_verification_email(user)
 
-        return Response({
-        "message": (
-            "Registration successful. "
-            "Please check your email. "
-            "to verify your account."
-        ),
-        "user_id": user.id,
-    },
-    status=status.HTTP_201_CREATED
-    )
+        return Response(
+            {
+                "message": ("Registration successful. " "Please check your email. " "to verify your account."),
+                "user_id": user.id,
+            },
+            status=status.HTTP_201_CREATED,
+        )
+
 
 class VerifyEmailView(APIView):
     permission_classes = [permissions.AllowAny]
 
     def get(self, request, token):
 
-    
         try:
             payload = decode_verification_token(token)
-        
+
         except signing.SignatureExpired:
 
-            return Response ({"error": "Verification link has expired."}, status=status.HTTP_400_BAD_REQUEST) 
-    
+            return Response(
+                {"error": "Verification link has expired."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
         except signing.BadSignature:
-            return Response ({"error": "Invalid verification token."}, status=status.HTTP_400_BAD_REQUEST)
-    
+            return Response(
+                {"error": "Invalid verification token."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
         user_id = payload.get("user_id")
         email = payload.get("email").lower().strip()
 
         try:
             user = User.objects.get(id=user_id, email=email)
         except User.DoesNotExist:
-            return Response ({"error":"User not found."}, status=status.HTTP_400_BAD_REQUEST)
-    
+            return Response({"error": "User not found."}, status=status.HTTP_400_BAD_REQUEST)
+
         if user.is_email_verified:
             return Response(
-                {
-                    "message": "Email already verified."
-                },
+                {"message": "Email already verified."},
                 status=status.HTTP_200_OK,
             )
 
@@ -76,13 +83,14 @@ class VerifyEmailView(APIView):
 
         return Response({"message": "Email verified successfully."}, status=status.HTTP_200_OK)
 
+
 class LoginView(APIView):
     permission_classes = [permissions.AllowAny]
 
     def post(self, request):
 
-        serializer = LoginSerializer(data = request.data)
-        serializer.is_valid(raise_exception = True)
+        serializer = LoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
 
         user = serializer.validated_data["user"]
 
@@ -90,12 +98,12 @@ class LoginView(APIView):
 
         return Response(
             {
-                "access" : str(refresh.access_token), 
-                "refresh" : str(refresh),      
+                "access": str(refresh.access_token),
+                "refresh": str(refresh),
             },
-            status=status.HTTP_200_OK
+            status=status.HTTP_200_OK,
         )
-    
+
 
 class ProfileView(APIView):
     permission_classes = [permissions.IsAuthenticated]
@@ -103,29 +111,30 @@ class ProfileView(APIView):
     def get(self, request):
         serializer = UserProfileSerializer(request.user)
         return Response(serializer.data)
-    
+
     def put(self, request):
         """Update user address"""
         user = request.user
-        address_data = request.data.get('address')
-        
-        if not address_data:
-            return Response(
-                {"error": "Address data is required."},
-                status= status.HTTP_400_BAD_REQUEST
-            )
-        
-        try:
-            address = user.address
-            serializer = AddressSerializer(address, data=address_data, partial=True)
-        except Address.DoesNotExist:
-            serializer = AddressSerializer(data=address_data)
+        address_data = request.data.get("address")
 
-        if serializer.is_valid():
-            serializer.save(user=user)
-            return Response(
-                UserProfileSerializer(user).data, status=status.HTTP_200_OK
-            )
+        # if not address_data:
+        #     return Response(
+        #         {"error": "Address data is required."},
+        #         status= status.HTTP_400_BAD_REQUEST
+        #     )
+
+        # try:
+        #     address = user.address
+        #     serializer = AddressSerializer(address, data=address_data, partial=True)
+        # except Address.DoesNotExist:
+        #     serializer = AddressSerializer(data=address_data)
+
+        # if serializer.is_valid():
+        #     serializer.save(user=user)
+        #     return Response(
+        #         UserProfileSerializer(user).data, status=status.HTTP_200_OK
+        #     )
+        return Response({"message": "Address updated successfully."}, status=status.HTTP_200_OK)
 
     def delete(self, request):
         """Delete user address"""
@@ -136,50 +145,51 @@ class ProfileView(APIView):
         except Address.DoesNotExist:
             return Response(
                 {"error": "User has no address to delete."},
-                status=status.HTTP_404_NOT_FOUND
+                status=status.HTTP_404_NOT_FOUND,
             )
         address.delete()
-        return Response(
-            {"message": "Address deleted successfully."},
-                status=status.HTTP_200_OK
-            )
+        return Response({"message": "Address deleted successfully."}, status=status.HTTP_200_OK)
 
     def patch(self, request):
 
-        serializer = UserProfileSerializer(request.user,data=request.data,partial=True,)
+        serializer = UserProfileSerializer(
+            request.user,
+            data=request.data,
+            partial=True,
+        )
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+
 
 class ProfilePhotoView(APIView):
 
     permission_classes = [permissions.IsAuthenticated]
     parser_classes = [MultiPartParser, FormParser]
-    def patch(self,request):
 
-        serializer = ProfilePhotoSerializer(request.user , data = request.data , partial =True)
+    def patch(self, request):
+
+        serializer = ProfilePhotoSerializer(request.user, data=request.data, partial=True)
         serializer.is_valid(raise_exception=True)
 
         if request.user.profile_photo:
             request.user.profile_photo.delete(save=False)
-        
+
         serializer.save()
-        return Response(
-            UserProfileSerializer(request.user).data , status=status.HTTP_200_OK
-        )
+        return Response(UserProfileSerializer(request.user).data, status=status.HTTP_200_OK)
 
-
-    def delete(self,request):
+    def delete(self, request):
         if not request.user.profile_photo:
             return Response(
-                {"error": "Profile photo does not exist."},status=status.HTTP_404_NOT_FOUND
+                {"error": "Profile photo does not exist."},
+                status=status.HTTP_404_NOT_FOUND,
             )
-        
+
         request.user.profile_photo.delete(save=False)
         request.user.profile_photo = None
-        request.user.save(update_fields =["profile_photo"])
+        request.user.save(update_fields=["profile_photo"])
 
         return Response(
-            {"message":"Profile photo deleted successfully."},status=status.HTTP_200_OK
+            {"message": "Profile photo deleted successfully."},
+            status=status.HTTP_200_OK,
         )
-    
